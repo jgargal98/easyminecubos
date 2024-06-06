150bb8d73bfefa49a6d4c25ada9a37f2acd08b1e
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-06 10:30:18 +0200

    buenos dias vietnam

--- website/propiedades.php
+++ website/propiedades.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang='es'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Propiedades - Easy Minecubos</title>
-    <link rel='stylesheet' href='properties.css'>
-</head>
-<body>
-    <div class='container'>
-        <img src='assets/easy-minecubos.png' alt='Título de la Página'>
-    </div>
-    <div class='container'>
-        <div class='formulario'>
-
 <?php
 session_start();
 
@@ -24,32 +9,36 @@ if (isset($_SESSION['usuario'])){
 
     while(!feof($properties)) {
 
-        //separamos el archivo en lineas para modificarlo
+        //separamos el archivo en líneas para modificarlo
         $linea = explode("< >", fgets($properties));
 
-        //Presentamos las opciones que a mi me interesan en un formulario
+        //Presentamos las opciones que a mí me interesan en un formulario
         $opcion = explode("=", $linea[0]);
 
         if (substr($opcion[0],0,1) != "#") {
             switch (trim($opcion[0])) {
                 case "difficulty":
-                    print $opcion[0] . ": 
-                    <select name='" . $opcion[0] . "' class='minecraft-select'>
-                        <option value='peaceful'>peaceful</option>
-                        <option value='easy'>easy</option>
-                        <option value='normal'>normal</option>
-                        <option value='hard'>hard</option>
-                        </select><br><br>";
+                    print "<div class='form-row'>
+                            <label for='" . $opcion[0] . "'>" . $opcion[0] . ":</label>
+                            <select name='" . $opcion[0] . "' class='minecraft-select'>
+                                <option value='peaceful'>peaceful</option>
+                                <option value='easy'>easy</option>
+                                <option value='normal'>normal</option>
+                                <option value='hard'>hard</option>
+                            </select>
+                        </div>";
                 break;
                 
                 case "gamemode":
-                    print $opcion[0] . ": 
-                    <select name='" . $opcion[0] . "' class='minecraft-select'>
-                        <option value='survival'>survival</option>
-                        <option value='creative'>creative</option>
-                        <option value='adventure'>adventure</option>
-                        <option value='spectator'>spectator</option>
-                        </select><br><br>";
+                    print "<div class='form-row'>
+                            <label for='" . $opcion[0] . "'>" . $opcion[0] . ":</label>
+                            <select name='" . $opcion[0] . "' class='minecraft-select'>
+                                <option value='survival'>survival</option>
+                                <option value='creative'>creative</option>
+                                <option value='adventure'>adventure</option>
+                                <option value='spectator'>spectator</option>
+                            </select>
+                        </div>";
                 break;
 
                 case "spawn-protection":
@@ -57,29 +46,34 @@ if (isset($_SESSION['usuario'])){
                 case "motd":
                 case "simulation-distance":
                 case "view-distance":
-                    print $opcion[0] . ": 
-                    <input type='text' name='" . $opcion[0] . "' value='" . $opcion[1] . "'><br><br>";
+                    print "<div class='form-row'>
+                            <label for='" . $opcion[0] . "'>" . $opcion[0] . ":</label>
+                            <input type='text' name='" . $opcion[0] . "' value='" . $opcion[1] . "'>
+                        </div>";
                 break;
                 
                 default:
                     //Opciones booleanas true-false
                     if (trim($opcion[1]) === "true" || trim($opcion[1]) === "false") {
-                        //entonces será un select de true o false, con opcion preseleccionada
-                        print $opcion[0] . ": 
-                        <select name='" . $opcion[0] . "' class='minecraft-select'>";
+                        //entonces será un select de true o false, con opción preseleccionada
+                        print "<div class='form-row'>
+                                <label for='" . $opcion[0] . "'>" . $opcion[0] . ":</label>
+                                <select name='" . $opcion[0] . "' class='minecraft-select'>";
                         
                         switch (trim($opcion[1])) {
                             case "true":
                                 print
                                     "<option value='true' selected='selected'>true</option>
                                     <option value='false'>false</option>
-                                    </select><br><br>";
+                                    </select>
+                                </div>";
                             break;
                             case "false":
                                 print 
                                     "<option value='true'>true</option>
                                     <option value='false' selected='selected'>false</option>
-                                    </select><br><br>";
+                                    </select>
+                                </div>";
                             break;
                         }
                     }
@@ -96,13 +90,7 @@ if (isset($_SESSION['usuario'])){
 }
 else
 {
-  print '<p>No estas conectado.</p>';
+  print '<p>No estás conectado.</p>';
 }
 
 ?>
-
-        </div>
-        <a href='logout.php' class='back-to-home'>Salir</a>
-    </div>
-</body>
-</html>
