3be3eb5149229d0d4a5efd11b255c6f7888d176b
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-13 18:53:13 +0200

    ultimo empujón

--- website/escribir_propiedades.php
+++ website/escribir_propiedades.php
@@ -1,20 +1,18 @@
 <?php
 require "vendor/autoload.php";
 
-use phpseclib\Crypt\RSA;
-use phpseclib\Net\SFTP;
+use phpseclib3\Crypt\PublicKeyLoader;
+use phpseclib3\Net\SFTP;
 
 $host = '34.202.66.61'; // Dirección IP o nombre de host SSH
 $username = 'ec2-user'; // Nombre de usuario SSH
 $private_key_file = '/home/ec2-user/easyminecubos-servermc.pem'; // Ruta a tu clave privada
 
+// create new SFTP instance
 $sftp = new SFTP($host);
 
 // create new RSA key
-$privateKey = new RSA();
-
-// load the private key
-$privateKey->loadKey(file_get_contents($private_key_file));
+$privateKey = PublicKeyLoader::load(file_get_contents($private_key_file));
 
 // login via sftp
 if (!$sftp->login($username, $privateKey)) {
@@ -22,4 +20,9 @@ if (!$sftp->login($username, $privateKey)) {
 }
 
 // create a remote new file with defined content
-$sftp->put('santino.txt', 'santino chupa');
+if (!$sftp->put('santino.txt', 'santino chupa')) {
+    throw new Exception('Error al crear archivo remoto');
+}
+
+echo "Archivo 'santino.txt' creado correctamente en el servidor SFTP.\n";
+?>
