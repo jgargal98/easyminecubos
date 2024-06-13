fb76b705e28884c73d66c7a4d1dd4d73b4f183b7
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-13 18:25:02 +0200

    ultimo empujón

--- website/escribir_propiedades.php
+++ website/escribir_propiedades.php
@@ -42,24 +42,20 @@ try {
     }
 
     try {
-        // Crear una instancia de SSH2
+                // Crear una instancia de SSH2
         $ssh = new SSH2($host, $port);
-    
-        // Leer la clave privada y cargarla en RSA
-        $key = new RSA();
-        $key->loadKey(file_get_contents($private_key));
-    
+
+        // Leer la clave privada
+        $key = file_get_contents($private_key);
+
         // Intentar autenticarse con la clave privada
         if (!$ssh->login($username, $key)) {
             throw new Exception('Login SSH fallido. Detalles: ' . $ssh->getLastError());
         }
-    
-        // Resto del código para generar el archivo y transferirlo
-    
     } catch (Exception $e) {
         echo 'Error: ' . $e->getMessage();
     }    
-
+    
     // Contenido de Docker Compose
     $container_name = $user . "-server";
     $docker_compose_content = "
