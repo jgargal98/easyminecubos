af7e5b2ed44e33f2e02628c77036114175750a3c
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-13 17:48:28 +0200

    ultimo empujón

--- website/escribir_propiedades.php
+++ website/escribir_propiedades.php
@@ -1,20 +1,73 @@
 <?php
+include "../inc/dbinfo.inc";
+
+require 'vendor/autoload.php'; // Autoload de Composer
+
+use phpseclib3\Net\SSH2;
+use phpseclib3\Crypt\RSA;
+use phpseclib3\Net\SCP;
+
+$host = '34.202.66.61';
+$port = 22; // Puerto por defecto para SSH
+$username = 'ec2-user';
 $private_key = '/home/ec2-user/easyminecubos-servermc.pem'; // Ruta a tu clave privada
+$passphrase = null; // Si tu clave tiene una frase de paso, añádela aquí
 
-echo "Verificando la clave privada...<br>";
+// Leer la clave privada
+$key = new RSA();
+$key->load(file_get_contents($private_key));
 
-if (!file_exists($private_key)) {
-    exit('Error: No se encuentra la clave privada en ' . $private_key . '<br>');
+session_start();
+
+$user = $_SESSION['usuario'];
+
+$file ="/var/www/dockercomposes/" . $user . "-docker-compose.yml";
+
+// Ruta del archivo local y destino remoto
+$localFile = $file;
+$remoteFile = "/home/ec2-user/docker/" . $user . "-docker-compose.yml";
+
+if (!file_exists("$file")) {
+    touch("$file");
 }
 
-echo "Ruta de la clave privada: $private_key<br>";
+fopen("$file", "w");
 
-$key_content = file_get_contents($private_key);
-if ($key_content === false) {
-    $error = error_get_last();
-    echo "Error al leer la clave privada: " . $error['message'] . "<br>";
-    exit();
-} else {
-    echo "Contenido de la clave privada:<br>";
-    echo "<pre>" . htmlspecialchars($key_content) . "</pre><br>";
+$container_name = $user . "-server";
+
+$docker_compose_content = "
+version: '3.8'
+services:
+    minecraft_server:
+        build: .
+        image: easy-minecubos
+        container_name: $container_name
+        ports:
+            - '25565:25565'
+        environment:
+";
+
+foreach ($_POST as $key => $value) {
+    $value = preg_replace("/[^a-zA-Z0-9\s]/", "", $value);
+    $docker_compose_content .= "            - $key = $value\n";
+}
+
+if (file_put_contents($file, $docker_compose_content) !== false) {
+    echo "Archivo $file generado correctamente.<br>";
+
+    // Crear una instancia de SSH2
+    $ssh = new SSH2($host, $port);
+
+    if (!$ssh->login($username, $key)) {
+        exit('Login Failed');
+    }
+
+    // Crear una instancia de SCP
+    $scp = new SCP($ssh);
+    // Transferir el archivo
+    if ($scp->put($remoteFile, file_get_contents($localFile))) {
+        echo "Archivo transferido correctamente.\n";
+    } else {
+        echo "Error al transferir el archivo.\n";
+    }
 }
