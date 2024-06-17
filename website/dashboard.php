45038bd07bb3e306aaba21dde9ea4fd5aea481ac
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-17 12:38:36 +0200

    ubicacion de los archivos inc

--- website/dashboard.php
+++ website/dashboard.php
@@ -7,36 +7,32 @@
     <link rel='stylesheet' href='properties.css'>
 </head>
 <body>
-    <div class='container'>
-        <img src='assets/easy-minecubos.png' alt='Título de la Página'>
-    </div>
-    <div class='container'>
-        
-<?php
-session_start();
-
-if (isset($_SESSION['usuario'])){
-
-    if ("variable" == false) {
-        print "Tu servidor está encendido, debes detenerlo para poder configurarlo.<br><br>";
-    }else {
-        print " <div class='options'>
-                    <a href='propiedades.php' class='minecraft-button'>Propiedades</a>
-                    <a href='bans.php' class='minecraft-button'>vetados</a>
-                    <a href='ops.php' class='minecraft-button'>Operadores</a>
-                    <a href='whitelist.php' class='minecraft-button'>whitelist</a>
-                </div>";
-    }
-}
-else
-{
-  print '<p>No estas conectado.</p>';
-}
-
-?>
+    <div class="center-wrapper">
+        <div class='container'>
+            <img src='assets/easy-minecubos.png' alt='Título de la Página'>
         </div>
         <div class='container'>
-        <a href='logout.php' class='back-to-home'>Salir</a>
+            <?php
+            session_start();
+            if (isset($_SESSION['usuario'])){
+                if ("variable" == false) {
+                    print "Tu servidor está encendido, debes detenerlo para poder configurarlo.<br><br>";
+                } else {
+                    print "<div class='options'>
+                            <a href='propiedades.php' class='minecraft-button'>Propiedades</a>
+                            <a href='bans.php' class='minecraft-button'>vetados</a>
+                            <a href='ops.php' class='minecraft-button'>Operadores</a>
+                            <a href='whitelist.php' class='minecraft-button'>whitelist</a>
+                           </div>";
+                }
+            } else {
+                print '<p>No estas conectado.</p>';
+            }
+            ?>
+        </div>
+        <div class='container'>
+            <a href='logout.php' class='back-to-home'>Salir</a>
+        </div>
     </div>
 </body>
 </html>
