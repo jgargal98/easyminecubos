33f446ff096d78e4671ac607a4a01a5d17241637
meanoentucorro <juanillogarcia14@gmail.com>
2024-06-17 13:05:47 +0200

    apariencia del dashboard

--- container/run_server.sh
+++ container/run_server.sh
@@ -9,11 +9,19 @@ fi
 # Nombre del archivo a utilizar
 FILE_NAME=$1
 
+# Nombre del contenedor basado en el argumento proporcionado
+CONTAINER_NAME="minecraft_server_${FILE_NAME}"
+
 # Construye la imagen Docker si no está construida
 docker build -t minecraft-server -f docker/easy-minecubos .
 
+# Verifica si ya existe un contenedor con el mismo nombre y lo elimina si existe
+if [ $(docker ps -a -q -f name=${CONTAINER_NAME}) ]; then
+    docker rm -f ${CONTAINER_NAME}
+fi
+
 # Ejecuta el contenedor
-docker run -d --name minecraft_server -e FILE_NAME=$FILE_NAME -v $(pwd)/properties:/host -p 25565:25565 minecraft-server
+docker run -d --name ${CONTAINER_NAME} -e FILE_NAME=$FILE_NAME -v $(pwd)/properties:/host -p 25565:25565 minecraft-server
 
 # Obtener la IP de la máquina
 IP=$(hostname -I | awk '{print $1}')
